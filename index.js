66174b4947150fe2417711f0165e2f0c1806a382
Ross Miller <rossalanmiller@att.net>
2019-09-20 18:01:48 -0400

    return ids of rows inserted

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
+
 const express = require('express')
 const bodyPareser = require('body-parser')
 const db = require('./queries')
 const app = express()
-const port = 3000
+const port = 80
 
+// Express Middleware
+const helmet = require('helmet') // creates headers that protect from attacks (security)
+const cors = require('cors')  // allows/disallows cross-site communication
+const morgan = require('morgan') // logs requests
+
+
+
+
+
+
+// App Middleware
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(helmet())
+app.use(cors(corsOptions))
+app.use(morgan('combined')) // use 'tiny' or 'combined'
 app.use(bodyPareser.json())
 app.use(bodyPareser.urlencoded({
             extended: true})
 )
 
-app.get('/api', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres Api'})
+app.get('/api', (req, res) => {
+    res.json({ info: 'Node.js, Express, and Postgres Api'})
 })
 
+// requests for the api
+
+// requests for the gui
+app.get('/', (req, res) => res.send('hello world'))
 app.get('/api/partReport', db.getReports)
-app.get('/api/partReport/:id', db.getReportByOrderId)
-app.post('/api/partReport', db.createReport)
+app.post('/api/partReport', db.postReport)
+app.post('/api/partReport2', db.postReport2)
+app.put('/api/partReport', db.putReport)
+app.delete('/api/partReport', db.deleteReport)
 
 process.on('uncaughtException', (exception) => {
     console.log(exception)
--- queries.js
+++ queries.js
@@ -1,4 +1,7 @@
 
+
+// use process.env variables to keep private variables,
+require('dotenv').config()
 const orders_table = 'invscanner_db.partreports'
 const Pool = require('pg').Pool
 const pg_format = require('pg-format');
@@ -14,7 +17,7 @@ const pool = new Pool({
 
 const getReports = (request, response) => {
     console.log(request.url);
-    pool.query(`SELECT * FROM ${orders_table} ORDER BY partreportid ASC`, (error, results) => {
+    pool.query(`SELECT * FROM ${orders_table}`, (error, results) => {
         if(error){
             console.error(error);
             response.status(500).json(error);
@@ -28,7 +31,7 @@ const getReports = (request, response) => {
 
 const getReportByOrderId = (request, response) => {
     console.log(request.url);
-    const id = parseInt(request.params.id);
+    const order_id = parseInt(request.params.id);
     var q = pool.query(`SELECT * FROM ${orders_table} WHERE order_id = $1`, [id], (error,results) =>{
         if(error){
             console.error(error);
@@ -41,14 +44,21 @@ const getReportByOrderId = (request, response) => {
     });
 }
 
-const createReport = (request, response) => {
-    console.log("CREATE_REPORT")
+const postReport = (request, response) => {
+    console.log("CREATE_REPORT");
     console.log(request.body);
+    var items = request.body
     
     var values = []
-    for(i = 0; i < request.body.length; i++)
+    if(!items.length)
     {
-        var item = request.body[i];
+        console.log('Assuming one item')
+        items = [request.body]
+    }
+    
+    for(i = 0; i < items.length; i++)
+    {
+        var item = items[i];
         values.push([
             item.order_id,
             item.order_name,
@@ -64,10 +74,10 @@ const createReport = (request, response) => {
                 (order_id, order_name, on_or_off, switch_status, radio_selection, spinner_selection)
                 VALUES
                 %L
-                RETURNING *
-                `;
+                RETURNING *`;
 
     const query2 = pg_format(query, values)
+    console.log(query2);
 
     pool.query(query2, (error, results) => {
         if(error){
@@ -76,15 +86,111 @@ const createReport = (request, response) => {
         }
         else
         {
-            response.status(201).json(results.rows[0]);
+            response.status(201).json(results.rows);
         }
     })
+}
 
+const postReport2 = (request, response) => {
+    console.log("CREATE_REPORT2");
+    console.log(request.body);
+    var items = request.body
+    
+    var values = []
+    var ids = []
+    if(!items.length)
+    {
+        console.log('Assuming one item')
+        items = [request.body]
+    }
+    
+    for(i = 0; i < items.length; i++)
+    {
+        var item = items[i];
+        values.push([
+            item.order_id,
+            item.order_name,
+            item.on_or_off,
+            item.switch_status,
+            item.radio_selection,
+            item.spinner_selection
+        ]);
+        ids.push(item.id);
+    }
 
+    var query = `
+                INSERT INTO ${orders_table} 
+                (order_id, order_name, on_or_off, switch_status, radio_selection, spinner_selection)
+                VALUES
+                %L
+                RETURNING *`;
+
+    const query2 = pg_format(query, values)
+    console.log(query2);
+
+    pool.query(query2, (error, results) => {
+        if(error){
+            console.error(error);
+            response.status(500).json(error);
+        }
+        else
+        {
+            response.status(201).json(ids);
+        }
+    })
+}
+
+const putReport = (request, response) => {
+    console.log('PUT_REPORT');
+    console.log(request.body);
+
+    var query = `UPDATE ${orders_table} 
+                    SET order_id = $2,
+                        order_name = $3,
+                        on_or_off = $4,
+                        switch_status = $5,
+                        radio_selection = $6,
+                        spinner_selection = $7
+                WHERE id = $1
+                RETURNING *`;
+
+    const b = request.body;
+    const args = [b.id, b.order_id, b.order_name, b.on_or_off, b.switch_status, b.radio_selection, b.spinner_selection];
+
+    pool.query(query, args, (error, results) =>{
+        if(error){
+            console.error(error)
+            response.status(400).json(error)
+        }
+        else{
+            response.json(results.rows)
+        }
+    })
+}
+
+const deleteReport = (request, response) => {
+    console.log("DELETE_REPORT");
+    console.log(request.body);
+    const id = parseInt(request.body.id);
+
+    var query = `DELETE FROM ${orders_table}
+                 WHERE id = $1`
+    pool.query(query, [id], (error, results) => {
+        if(error){
+            console.error(err);
+            response.status(400).json({dbError: 'failed delete'})
+        }
+        else{
+            response.json({delete: 'true'})
+        }
+    })
 }
 
 module.exports = {
     getReports,
     getReportByOrderId,
-    createReport
+    postReport,
+    postReport2,
+    putReport,
+    deleteReport
 }
